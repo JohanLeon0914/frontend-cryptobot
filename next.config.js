8a8b986418e554931e2336826ce32db7a3305f46
JohanLeon0914 <johanalbertols@ufps.edu.co>
2022-11-29 19:06:00 -0500

    agregando links en variables de entorno

--- next.config.js
+++ next.config.js
@@ -6,11 +6,11 @@ const nextConfig = {
 
 module.exports = {
   env: {
-    EXCHANGE: "https://e2ac-181-132-2-224.ngrok.io",
-    NEWS: "https://e2ac-181-132-2-224.ngrok.io",
-    ACCOUNTS: "https://e2ac-181-132-2-224.ngrok.io",
-    MESSAGING: "https://e2ac-181-132-2-224.ngrok.io",
-    QA: "https://e2ac-181-132-2-224.ngrok.io",
+    EXCHANGE: "http://34.125.172.125:5000",
+    NEWS: "http://34.125.68.73:5000",
+    ACCOUNTS: "http://34.125.117.244:5000",
+    MESSAGING: "http://34.125.244.168:8080",
+    QA: "http://34.125.238.86:9000",
   },
   images: {
     unoptimized: true,
