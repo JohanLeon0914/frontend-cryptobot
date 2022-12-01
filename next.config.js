19cd0b87195ccd2cf113de651fde5b0686a7e77f
JohanLeon0914 <johanalbertols@ufps.edu.co>
2022-12-01 00:00:39 -0500

    corrigiendo

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
 module.exports = {
   env: {
     EXCHANGE: "https://48de-181-132-2-224.ngrok.io",
-    NEWS: "https://this-is-cryptobot.online",
+    NEWS: "https://this-is-crypto-bot.online",
     ACCOUNTS: "https://this-is-crypto-bot.online",
     MESSAGING: "https://ea64-181-132-0-23.ngrok.io",
     QA: "https://this-is-crypto-bot.online",
