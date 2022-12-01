fdffd7936b2248dbb67ac74825d3af14c5234170
JohanLeon0914 <johanalbertols@ufps.edu.co>
2022-11-30 23:30:13 -0500

    host de news

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
 module.exports = {
   env: {
     EXCHANGE: "https://48de-181-132-2-224.ngrok.io",
-    NEWS: "https://488b-179-32-182-204.ngrok.io",
+    NEWS: "https://this-is-cryptobot.online",
     ACCOUNTS: "https://this-is-crypto-bot.online",
     MESSAGING: "https://ea64-181-132-0-23.ngrok.io",
     QA: "https://this-is-crypto-bot.online",
