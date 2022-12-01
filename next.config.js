6f682678d032e02342e799df86726a7654eb81bf
JohanLeon0914 <johanalbertols@ufps.edu.co>
2022-11-30 21:51:12 -0500

    volviendo a ngroc

--- next.config.js
+++ next.config.js
@@ -6,10 +6,10 @@ const nextConfig = {
 
 module.exports = {
   env: {
-    EXCHANGE: "https://this-is-crypto-bot.online",
-    NEWS: "https://this-is-crypto-bot.online",
+    EXCHANGE: "https://48de-181-132-2-224.ngrok.io",
+    NEWS: "https://488b-179-32-182-204.ngrok.io",
     ACCOUNTS: "https://this-is-crypto-bot.online",
-    MESSAGING: "https://this-is-crypto-bot.online",
+    MESSAGING: "https://ea64-181-132-0-23.ngrok.io",
     QA: "https://this-is-crypto-bot.online",
   },
   images: {
