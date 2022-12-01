4316751bfc7a28a7caae4ad425df04b2b5fe7734
JohanLeon0914 <johanalbertols@ufps.edu.co>
2022-12-01 00:09:02 -0500

    messaging

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = {
     EXCHANGE: "https://48de-181-132-2-224.ngrok.io",
     NEWS: "https://this-is-crypto-bot.online",
     ACCOUNTS: "https://this-is-crypto-bot.online",
-    MESSAGING: "https://ea64-181-132-0-23.ngrok.io",
+    MESSAGING: "https://this-is-crypto-bot.online",
     QA: "https://this-is-crypto-bot.online",
   },
   images: {
