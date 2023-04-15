14e04894baf21f16034eedd6c2049eea7790798c
JohanLeonMilenium <123989046+JohanLeonMilenium@users.noreply.github.com>
2023-04-15 09:55:21 -0500

    agregando variables de entorno

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ const nextConfig = {
 module.exports = {
   env: {
     EXCHANGE: "https://cryptobot-345516.ue.r.appspot.com",
-    NEWS: "https://accounts-dot-cryptobot-345516.ue.r.appspot.com",
-    ACCOUNTS: "https://this-is-crypto-bot.online",
+    NEWS: "https://news-dot-cryptobot-345516.ue.r.appspot.com",
+    ACCOUNTS: "https://accounts-dot-cryptobot-345516.ue.r.appspot.com",
     MESSAGING: "https://ea64-181-132-0-23.ngrok.io",
     QA: "https://qa-dot-cryptobot-345516.ue.r.appspot.com",
   },
