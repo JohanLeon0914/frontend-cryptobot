c557951ca58c52c834fbf5e4108b491e3dfe51a5
JohanLeonMilenium <123989046+JohanLeonMilenium@users.noreply.github.com>
2023-04-15 10:00:26 -0500

    agregando variable de mensajeria

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = {
     EXCHANGE: "https://cryptobot-345516.ue.r.appspot.com",
     NEWS: "https://news-dot-cryptobot-345516.ue.r.appspot.com",
     ACCOUNTS: "https://accounts-dot-cryptobot-345516.ue.r.appspot.com",
-    MESSAGING: "https://ea64-181-132-0-23.ngrok.io",
+    MESSAGING: "https://messaging-dot-cryptobot-345516.ue.r.appspot.com",
     QA: "https://qa-dot-cryptobot-345516.ue.r.appspot.com",
   },
   images: {
