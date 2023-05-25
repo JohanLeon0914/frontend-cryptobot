a949ab0453828e254f3eea88cf305b7943584d2d
Johan León <64605783+JohanLeon0914@users.noreply.github.com>
2023-05-25 13:32:22 -0500

    agregando tokenn

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: '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',
 });
 const openai = new OpenAIApi(configuration);
 
